d7e6c6d3b37e0f6971ee63d4194284a751f086a6
Pandit <spand7@uisad.uis.edu>
2021-06-29 13:51:43 -0500

    6/29 added hamburger menu

--- js/directives/icons.js
+++ js/directives/icons.js
@@ -23,3 +23,8 @@ body.directive("downLoad", function(){
         templateUrl:'../svgs/download.svg'
     };
 });
+body.directive("hamburgerMenu", function(){
+    return{
+        templateUrl:'../svgs/hamburgermenu.svg'
+    };
+});
