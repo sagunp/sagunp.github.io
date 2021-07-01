115e0bd0adbc103ae22017dc972852ce5a65cbab
Pandit <spand7@uisad.uis.edu>
2021-07-01 11:36:33 -0500

    changed some stuff on bubblesort and added counts

--- js/sorting/bubblesort.js
+++ js/sorting/bubblesort.js
@@ -1,16 +1,25 @@
 async function bubblesort(){
+    let arr = 0; 
+    let comp = 0;
     for (var i = 0; i < 59; i++){
         for(var j = 0; j < 59 - i; j++){
-            let a = document.getElementById("bar" + j);
             let c = j + 1;
+
+            let a = document.getElementById("bar" + j);
             let b = document.getElementById("bar" + c);
+
             a.style.background = "blue";
             b.style.background = "blue";
-            
+
             if(parseInt(a.style.height) > parseInt(b.style.height)){
                 await producing();
                 swap(a, b);
             }
+
+            arr +=2;
+            comp +=1;
+            updateAccessesAndComparisions(arr, comp);
+
             a.style.background = "black";
             b.style.background = "black";
         }
@@ -18,4 +27,14 @@ async function bubblesort(){
         document.getElementById("bar" + x).style.background = "rgba(238, 231, 231, 0.945)";
     }
     document.getElementById("bar0").style.background = "rgba(238, 231, 231, 0.945)";
+}
+
+async function runBubbleSort(){
+    if(document.getElementById("bubbleB").disabled){
+        alert("Bubble sort is running, please wait or refresh the page to terminate.")
+    }else{
+        disableContainer();
+        await bubblesort();
+        enableContainer();
+    }
 }
--- js/sorting/controller.js
+++ js/sorting/controller.js
@@ -18,7 +18,7 @@ function randomize(){
     clearContainer();
 
     for (var i = 0; i < 60; i++){
-        let randomNumber = Math.floor(Math.random() * (500 - 50) + 10);
+        let randomNumber = Math.floor(Math.random() * (400 - 50) + 10);
         var element = document.createElement("div");
         element.classList.add("lines");
         element.setAttribute("id", "bar" + i);
@@ -30,6 +30,35 @@ function randomize(){
 
 function clearContainer(){
     container.innerHTML="";
+    document.getElementById("arraycount").innerHTML= 0;
+    document.getElementById("comparisions").innerHTML= 0;
 }
 
-let producing = () => new Promise(resolve => setTimeout(resolve, 500));
+
+function disableContainer(){
+    document.getElementById("countingB").disabled = true;
+    document.getElementById("heapB").disabled = true;
+    document.getElementById("mergeB").disabled = true;
+    document.getElementById("quickB").disabled = true;
+    document.getElementById("insertionB").disabled = true;
+    document.getElementById("selectionB").disabled = true;
+    document.getElementById("bubbleB").disabled = true;
+}
+
+function enableContainer(){
+    document.getElementById("countingB").disabled = false;
+    document.getElementById("heapB").disabled = false;
+    document.getElementById("mergeB").disabled = false;
+    document.getElementById("quickB").disabled = false;
+    document.getElementById("insertionB").disabled = false;
+    document.getElementById("selectionB").disabled = false;
+    document.getElementById("bubbleB").disabled = false;
+}
+
+function updateAccessesAndComparisions(arrayAccess, comparisions){
+    document.getElementById("arraycount").innerHTML=arrayAccess;
+    document.getElementById("comparisions").innerHTML=comparisions;
+}
+let producing = () => new Promise(resolve => setTimeout(resolve, 10));
+
+
