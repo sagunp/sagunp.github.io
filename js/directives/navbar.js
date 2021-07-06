88a70bd22723806eb21bc5b289d9c290c83768aa
Pandit <spand7@uisad.uis.edu>
2021-07-06 01:53:56 -0500

    hamburger menu fixed 50 %

--- js/directives/navbar.js
+++ js/directives/navbar.js
@@ -3,3 +3,12 @@ body.directive("navBar", function(){
         templateUrl:'../js/directives/navbar.html'
     };
 });
+
+function togglebar(){
+    let a = document.querySelectorAll(".d-md-block");
+    console.log("check");
+
+    for(let i = 0; i < a.length; i++){
+      a[i].classList.toggle("d-none");
+    }
+  }
